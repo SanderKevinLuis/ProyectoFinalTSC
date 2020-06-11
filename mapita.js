382037661909a251c513bf6d45efc0b1f9dc3294
SanderKevinLuis <61296058+SanderKevinLuis@users.noreply.github.com>
2020-06-10 21:18:01 -0500

    Update mapita.js
    comentario

--- mapita.js
+++ mapita.js
@@ -14,7 +14,7 @@ L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 
-
+//estoy agregando un comentario
 
 function creaIcono(ruta, width, height) {
 	var icono = L.icon({
